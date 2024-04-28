ec95c9b7ff3d5bf0da9d68a99c828584c603442f
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-04-28 19:41:17 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log(`Server is running on port:${process.env.PORT || 3000}`);
   });
 }
 bootstrap();
