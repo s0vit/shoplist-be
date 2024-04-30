83d09ddb41988ad94c2b920903d391d92731ce2d
iPavka <ipavka@ya.ru>
2024-04-30 11:46:22 +0200

    Update Prettier configuration, add dependencies, update server port, and ignore files

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
   app.setGlobalPrefix('api');
-  await app.listen(3000, () => {
-    console.log(`Server is running on port:${process.env.PORT || 3000}`);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT || 3333}`);
   });
 }
+
 bootstrap();
