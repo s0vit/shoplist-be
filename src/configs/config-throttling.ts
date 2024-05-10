eff9defc382aff3bc5af06d126fd2975e12b94f2
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-05-10 19:20:47 +0200

    added nest throttle service
    add to .env:
    THROTTLE_TTL=5
    THROTTLE_LIMIT=5

---
+++ src/configs/config-throttling.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { seconds } from '@nestjs/throttler';
+
+export const configThrottling = (config: ConfigService) => {
+  const ttlInMilliseconds = seconds(config.get('THROTTLE_TTL'));
+  return {
+    // TODO add redis storage
+    // https://www.npmjs.com/package/nestjs-throttler-storage-redis
+    throttlers: [
+      {
+        ttl: ttlInMilliseconds,
+        limit: +config.get('THROTTLE_LIMIT'),
+      },
+    ],
+  };
+};
