d7c72de10a1d93a5c42b858f748b4858854626e4
iPavka <ipavka@ya.ru>
2024-05-16 10:10:41 +0200

    removed @IsNotEmpty()

--- src/auth/dto/refresh.dto.ts
+++ src/auth/dto/refresh.dto.ts
@@ -1,8 +1,7 @@
-import { IsJWT, IsNotEmpty } from 'class-validator';
+import { IsJWT } from 'class-validator';
 import { ERROR_AUTH } from '../constants/auth-constants.enum';
 
 export class RefreshDto {
-  @IsNotEmpty()
   @IsJWT({ message: ERROR_AUTH.AUTH_ERROR_TOKEN })
   refreshToken: string;
 }
