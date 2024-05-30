452c31116bbb45cd92d4faaa44a917b22e86f27c
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-05-30 21:50:30 +0200

    60-fix-swagger-production
    #60 added customCSS and customJS routes

--- src/configs/setupApp.ts
+++ src/configs/setupApp.ts
@@ -4,7 +4,6 @@ import { customValidationPipe } from '../pipes/customValidationPipe';
 import ErrorExceptionFilter from '../utils/exeptionFilters/errorExeptionFilter';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as serveStatic from 'serve-static';
 
 export const setupApp = async (app: INestApplication) => {
   const CLIENT_URLS = process.env.CLIENT_URLS;
@@ -26,6 +25,15 @@ export const setupApp = async (app: INestApplication) => {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/swagger', app, document);
-  app.use('/swagger', serveStatic('node_modules/swagger-ui-dist', { index: ['index.html'] }));
+  SwaggerModule.setup('/swagger', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 };
