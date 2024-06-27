fda75c599d02a26e63f5d7e0772dc117bbe81239
Aleksei Diuzhev <89312941+itbali@users.noreply.github.com>
2024-06-27 16:41:31 +0200

    #110 share-expenses-should-accept-empty-arrays (#124)
    Co-authored-by: Alex Duzhev <alexey.duzhev@tmrw.com>

--- src/app/access-control/decorators/is-optional-mongoid-array.decorator.ts
+++ src/app/access-control/decorators/is-optional-mongoid-array.decorator.ts
@@ -1,6 +1,6 @@
 import { applyDecorators } from '@nestjs/common';
-import { ArrayNotEmpty, IsArray, IsMongoId, IsOptional } from 'class-validator';
+import { IsArray, IsMongoId, IsOptional } from 'class-validator';
 
 export function IsOptionalMongoidArrayDec() {
-  return applyDecorators(IsOptional(), IsArray(), ArrayNotEmpty(), IsMongoId({ each: true }));
+  return applyDecorators(IsOptional(), IsArray(), IsMongoId({ each: true }));
 }
