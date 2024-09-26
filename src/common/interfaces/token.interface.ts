709c4a370b314af30b82d62fd40c9e6d838cc74d
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-09-26 13:51:14 +0200

    first iteration will go only with Google

--- src/common/interfaces/token.interface.ts
+++ src/common/interfaces/token.interface.ts
@@ -9,3 +9,40 @@ export interface TokenPayload {
 export interface CustomRequest extends Request {
   user: TokenPayload;
 }
+
+export interface GoogleRequest extends Request {
+  user: GoogleProfile;
+}
+
+export interface GoogleProfile {
+  email: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export type GoogleResponse = {
+  id?: string;
+  displayName?: string;
+  name?: {
+    familyName?: string;
+    givenName?: string;
+  };
+  emails?: {
+    value?: string;
+    verified?: boolean;
+  }[];
+  photos?: {
+    value?: string;
+  }[];
+  provider?: string;
+  _raw?: string;
+  _json?: {
+    sub?: string;
+    name?: string;
+    given_name?: string;
+    family_name?: string;
+    picture?: string;
+    email?: string;
+    email_verified?: boolean;
+  };
+};
